06fbee4c2eeec336fce1e4547f3a1f9995b651b3
amadorj <amadorj@SRFKW2046>
2018-04-17 16:04:38 -0700

    Add project files.

---
+++ Alert/AlertType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Alert
+{
+    public class AlertType
+    {
+        #region Members
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public string Name { get; set; }
+        public string Param { get; set; }
+        #endregion
+    }
+}
---
+++ Alert/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlertType[] alerts = new AlertType[]
+            {
+                new AlertType
+                {
+                    Min = 1,
+                    Max = 10,
+                    Name = "Low",
+                    Param = "A"
+                },
+                new AlertType
+                {
+                    Min = 11,
+                    Max = 20,
+                    Name = "Medium",
+                    Param = "A"
+                },
+                new AlertType
+                {
+                    Min = 21,
+                    Max = 30,
+                    Name = "High",
+                    Param = "A"
+                },
+                new AlertType
+                {
+                    Min = 1,
+                    Max = 10,
+                    Name = "Low",
+                    Param = "B"
+                },
+                new AlertType
+                {
+                    Min = 11,
+                    Max = 20,
+                    Name = "Medium",
+                    Param = "B"
+                }
+            };
+
+            int value = 15;
+            string param = "A";
+
+            var alertType = alerts.Where(a => a.Param.Equals(param) && (value >= a.Min && value <= a.Max)).SingleOrDefault();
+            string alertName = alertType.Name;
+
+            Console.WriteLine("Name: " + alertName);
+        }
+    }
+}
